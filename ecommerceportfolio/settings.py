cec6cfc2eb9c0b8aad99c476b0b78214032f68bc
UtkuDenizSonmez <73853487+UtkuDenizSonmez@users.noreply.github.com>
2021-08-30 22:34:11 +0300

    asdasd

--- ecommerceportfolio/settings.py
+++ ecommerceportfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#şp$g+jğş9tax!#a3ğcup@1şüüüp$8obtş2_+&k3q+ğpmu)5%asj6ğğyjpkag')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#şp$g+sdfsdfjğş9tax!#asfdsdf3ğcup@1şüüdsfüp$8obtş2_+&k3q+ğpmu)5%asj6ğğyjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
